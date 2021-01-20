688449e3d67b9b424d815132263901cfd7451140
NikBenson <NikBenson@pm.me>
2021-01-20 21:20:24 +0100

    fixed old dependencies

--- src/main/java/com/github/NikBenson/RoleplayBot/modules/ModulesManager.java
+++ src/main/java/com/github/NikBenson/RoleplayBot/modules/ModulesManager.java
@@ -18,9 +18,7 @@ public class ModulesManager {
 	private static final Set<RoleplayBotModule> activeModules = new HashSet<>();
 
 	public static void registerModule(Class<? extends RoleplayBotModule> module) {
-		if(allModules.contains(module)) {
-			allModules.remove(module);
-		}
+		allModules.remove(module);
 		allModules.add(module);
 	}
 
@@ -37,6 +35,14 @@ public class ModulesManager {
 			module.unload(activeGuild);
 		}
 	}
+
+	public static void unloadModule(Class<? extends RoleplayBotModule> moduleClass, Guild guild) {
+		RoleplayBotModule module = getLoadedModule(moduleClass);
+		if(module != null) {
+			module.unload(guild);
+		}
+	}
+
 	private static void loadModules() {
 		File guildsDirectory = new File(ConfigurationManager.getInstance().getConfigurationRootPath(), ConfigurationPaths.GUILDS_DIRECTORY);
 		File[] guilds = guildsDirectory.listFiles();
@@ -47,7 +53,7 @@ public class ModulesManager {
 					Guild guild = Bot.getJDA().getGuildById(guildId);
 					JSONArray modules = (JSONArray) readJSONFromFile(new File(guildDirectory, ConfigurationPaths.MODULES_FILE));
 
-					ModulesManager.activateModules(guild, (String[]) modules.toArray(new String[0]));
+					ModulesManager.loadModules(guild, (String[]) modules.toArray(new String[0]));
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -55,7 +61,7 @@ public class ModulesManager {
 		}
 	}
 
-	public static Set<Class<? extends RoleplayBotModule>> getActive(Guild guild) {
+	public static Set<Class<? extends RoleplayBotModule>> getLoaded(Guild guild) {
 		Set<Class<? extends RoleplayBotModule>> active = new HashSet<>();
 		for(RoleplayBotModule module : activeModules) {
 			if(module.isActive(guild)) {
@@ -65,27 +71,43 @@ public class ModulesManager {
 
 		return active;
 	}
+	public static boolean isLoaded(String fullClassName, Guild guild) {
+		Class<? extends RoleplayBotModule> moduleClass = getModuleClassByName(fullClassName);
+		if(moduleClass != null) {
+			RoleplayBotModule module = getLoadedModule(moduleClass);
+			if(module != null) {
+				return module.isActive(guild);
+			}
+		}
 
-	public static void activateModules(Guild guild, String... moduleNames) {
+		return false;
+	}
+	public static RoleplayBotModule require(String module) {
+		return getLoadedModule(getModuleClassByName(module));
+	}
+
+	public static void loadModules(Guild guild, String... moduleNames) {
 		for(String moduleName : moduleNames) {
-			activateModule(guild, moduleName);
+			loadModule(guild, moduleName);
 		}
 	}
 
-	public static boolean activateModule(Guild guild, String name) {
+	public static boolean loadModule(Guild guild, String name) {
 		Class<? extends RoleplayBotModule> module = getModuleClassByName(name);
 		if(module != null) {
 			try {
-				activateModuleFromClass(guild, module);
+				loadModuleFromClass(guild, module);
 				return true;
-			} catch (Exception ignored) {}
+			} catch (Exception ignored) {
+				ignored.printStackTrace();
+			}
 		}
 
-		System.out.printf("could not activate module: %s%n", name);
+		System.out.printf("could not load module: %s%n", name);
 		return false;
 	}
-	private static void activateModuleFromClass(Guild guild, @NotNull Class<? extends RoleplayBotModule> moduleClass) throws IllegalAccessException, InstantiationException {
-		RoleplayBotModule module = getActiveModule(moduleClass);
+	private static void loadModuleFromClass(Guild guild, @NotNull Class<? extends RoleplayBotModule> moduleClass) throws IllegalAccessException, InstantiationException {
+		RoleplayBotModule module = getLoadedModule(moduleClass);
 		if(module == null) {
 			module = moduleClass.newInstance();
 			activeModules.add(module);
@@ -93,7 +115,7 @@ public class ModulesManager {
 		module.load(guild);
 	}
 
-	private static RoleplayBotModule getActiveModule(Class<? extends RoleplayBotModule> moduleClass) {
+	private static RoleplayBotModule getLoadedModule(Class<? extends RoleplayBotModule> moduleClass) {
 		for(RoleplayBotModule activeModule: activeModules) {
 			if(activeModule.getClass().equals(moduleClass)) {
 				return activeModule;
